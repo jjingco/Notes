3cf77fd9ad27d9defa560e44bee6b2bd5a88000e
jjingco <43177752+jjingco@users.noreply.github.com>
2018-09-26 14:47:21 -0600

    updated

---
+++ Sass/sassstartercode/startercode/02-sassproject/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+
+gulp.task('test', function() {
+    console.log('testing...')
+})
+
+gulp.task('style', function() {
+    gulp.src('./sass/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', ['style'])
+})
